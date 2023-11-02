b5a8f10f7143701c3321ecfb8406e9688a389bb6
Hugues Codeur <huguescodeur@gmail.com>
2023-11-02 11:10:28 +0100

    Logique Authentification 98%

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
-from flask import Flask, render_template, request, redirect, url_for
+import hashlib
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from flask import Flask, jsonify, render_template, request, redirect, session, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import form, FlaskForm
+from werkzeug.security import check_password_hash
+
 import pymysql
+from werkzeug.security import generate_password_hash
+from wtforms import PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 app = Flask(__name__)
+# DATABASE_URL = 'mysql://root:''@localhost/colab_zoro'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:''@localhost/colab_zoro'
 app.config['SECRET_KEY'] = 'secret key'
 
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
+bcrypt = Bcrypt(app)
 
 with app.app_context():
     db.create_all()
+
+
+# ? Création du modèle SQLAlchemy pour les utilisateurs
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(40), unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+# def reset_passwords():
+#     users = User.query.all()
+#     for user in users:
+#         user.set_password(user.password)
+#     db.session.commit()
+
+
+# reset_passwords()
+
+
+# ? Création du modèle SQLAlchemy pour les Magasins
 
 
 class Data(db.Model):
@@ -24,18 +62,104 @@ class Data(db.Model):
         return '<Name %r>' % self.id
 
 
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[
+        DataRequired(), Length(min=4, max=40)])
+    password = PasswordField('Password', validators=[
+        DataRequired(), Length(min=6, max=50)])
+    submit = SubmitField('Connexion')
 
-@app.route("/")
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[
+        DataRequired(), Length(min=4, max=40)])
+    password = PasswordField('Password', validators=[
+        DataRequired(), Length(min=6, max=50)])
+    submit = SubmitField('Inscription')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        # generate_password_hash(
+        # password, method='pbkdf2:sha256')
+
+        # ? Vérifier si le nom d'utilisateur est déjà pris
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash('Username already exit. Please choose another.', 'danger')
+            return redirect(url_for('register'))
+
+        # ? Créer un nouvel utilisateur
+        new_user = User(username=username)
+        new_user.set_password(password)
+        db.session.add(new_user)
+
+        try:
+            db.session.commit()
+            login_user(new_user)
+            flash('Registration successful. You can now log in.', 'success')
+            return redirect(url_for('choix'))
+        except Exception as e:
+            flash('An error occurred during registration. Please try again.', 'danger')
+            return redirect(url_for('register'))
+
+    return render_template('register.html', form=form)
+
+
+# ? Route `/login`
+@app.route("/", methods=["POST", "GET"])
 def login():
-    return render_template("zoro.html")
+    form = LoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.query.filter_by(username=username).first()
+        print(f"User found: {user}")
+        if user and user.check_password(password):
+            login_user(user)
+            flash('Login successful!', 'success')
+            print(f"username: {username} and password: {password}")
+            return redirect(url_for('choix'))
+            # redirect(url_for('choix'))
+        else:
+            flash(f'Login failed. Check your username: {
+                username} and password: {password}.', 'danger')
+            print("Login failed. Check your username and password.")
+            # return "Login failed. Check your username and password."
+    return render_template('zoro.html', form=form)
+
+
+# ? Route `/logout`
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    if request.method == 'POST':
+        logout_user()
+        flash('You have been logged out.', 'info')
+        return redirect(url_for('login'))
+    return redirect(request.url)
 
 
 @app.route("/choix/")
+@login_required
 def choix():
-    return render_template("choix.html")
+    flash('Registration successful. You can now log in.', 'success')
+    return render_template("choix.html")  # redirect(url_for("login"))
 
 
 @app.route("/magasin/")
+@login_required
 def magasin():
     # magasin = Data.query.filter_by(id=_id).first()
     magasins = Data.query.order_by(Data.id).all()
@@ -43,6 +167,7 @@ def magasin():
 
 
 @app.route("/new_magasin/", methods=['GET', 'POST'])
+@login_required
 def new_magasin():
     if request.method == 'GET':
         return render_template('new_magasin.html')
@@ -64,11 +189,9 @@ def success():
     magasins = Data.query.order_by(Data.id).all()
     return render_template("success.html", magasins=magasins)
 
-@app.route("/modifier_mag/")
-def modifier_mag():
-    return render_template("modifier_mag.html")
 
 @app.route("/modifier_mag/<int:_id>", methods=["POST", "GET"])
+@login_required
 def modifier_mag(_id):
     if request.method == 'POST':
         name = request.form["name"]
@@ -99,19 +222,17 @@ def supp_def(_id):
 
 @app.route("/supp_mag/<int:_id>")
 def supp_mag(_id):
-    magasins = Data.query.order_by(Data.id).all()
     magasin = Data.query.filter_by(id=_id).first()
     # db.session.delete(magasin)
     # db.session.commit()
-    return render_template("/supp_mag.html", magasins=magasins)
-
+    return render_template("/supp_mag.html", id_mag=magasin)
 
 
 @app.route("/success_supp/")
 def success_sup():
     magasins = Data.query.order_by(Data.id).all()
-    # magasin = Data.query.filter_by(id=_id).first()
-    # magasins=magasins
+    # mag_id = Data.query.filter_by(id=_id).first()
+    # flash_id = flash("{}".format(mag_id.id))
     return render_template("success_supp.html", magasins=magasins)
 
 
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\goliy\Desktop\developerData_IA-huguescodeur\DEV WEB\Flask Projects\zoroColaboration-main\venv
+set VIRTUAL_ENV=C:\Users\goliy\Desktop\developerData_IA-huguescodeur\DEV WEB\Flask Projects\zoroColaboration\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
